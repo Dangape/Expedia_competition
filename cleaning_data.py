364163cbdd90c8d69364031c45df860e184cfd74
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-04 02:08:18 -0300

    Update cleaning_data.py

--- cleaning_data.py
+++ cleaning_data.py
@@ -10,32 +10,38 @@ data = pd.read_csv("Data/training_set_VU_DM.csv")
 print(data.head())
 print(data.columns)
 
-#Plot missing values
+#Show missing values percentege for each column
 for column in data.columns:
     percent = (data[str(column)].isna().sum())/(len(data[str(column)]))*100
     print(str(column),":",float(percent))
 
-#Creating new features
-data["price_difference_history"] = data["prop_log_historical_price"] - np.log(data["price_usd"])
-data["price_difference_user"] = data["visitor_hist_adr_usd"] - data["price_usd"]
-
-unique_prop = data.prop_id.unique()
+unique_prop = data.prop_id.unique() #unique property IDs
 
 #Run this part just if you need to change the code, since it takes too long to finish the loop
-# for prop in tqdm(unique_prop):
-#     appearences = len(data[data.prop_id == prop]) #count appearences of a prop id
-#     booked = len(data[(data.prop_id==prop)&(data.booking_bool==int(1))]) #count how many times a prop_id was booked
-#     clicked = len(data[(data.prop_id==prop)&(data.click_bool==int(1))]) #count how many time a prop_id was clicked
-#     data.loc[data.prop_id==prop,"prob_booked"] = booked/appearences #probability of being booked
-#     data.loc[data.prop_id == prop, "prob_clicked"] = clicked/appearences #probability of being clicked
-#
-# print(data)
-#
-# #Saving new data frame
-# print("Saving file to disk...")
-# data.to_pickle("feature_engineering.pkl") #save dataframe with new features
-#print("File saved!")
+for prop in tqdm(unique_prop):
+    appearences = len(data[data.prop_id == prop]) #count appearences of a prop id
+    booked = len(data[(data.prop_id==prop)&(data.booking_bool==int(1))]) #count how many times a prop_id was booked
+    clicked = len(data[(data.prop_id==prop)&(data.click_bool==int(1))]) #count how many time a prop_id was clicked
+    data.loc[data.prop_id==prop,"prob_booked"] = booked/appearences #probability of being booked
+    data.loc[data.prop_id == prop, "prob_clicked"] = clicked/appearences #probability of being clicked
+
+#Creating new features
+data = data.drop(columns=["gross_booking_usd"]) #not an interesting feature
+data["price_difference_history"] = data["prop_log_historical_price"] - np.log(data["price_usd"])
+data["price_difference_user"] = data["visitor_hist_adr_usd"] - data["price_usd"]
+data["starrating_diff"] = data["visitor_hist_starrating"] - data["prop_starrating"]
+data = data.drop(columns=["visitor_hist_starrating"]) #drop after calculating starrating_diff
+
+for prop in tqdm(unique_prop):
+    data.loc[data.prop_id == prop,"prop_review_score"] = data.loc[data.prop_id == prop,"prop_review_score"].fillna(min(data.loc[data.prop_id==prop,"prop_review_score"]))
+
+#Saving new data frame
+print("Saving file to disk...")
+data.to_pickle("feature_engineering.pkl") #save dataframe with new features
+print("File saved!")
 
 #Load saved dataframe
+print("Loading data...")
 data = pd.read_pickle("feature_engineering.pkl")
+print("Loaded!")
 print(data)
