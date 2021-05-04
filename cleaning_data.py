862cc7b2c2986e3e6d4e65abb00268573f4dfca8
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-03 23:08:13 -0300

    commented

--- cleaning_data.py
+++ cleaning_data.py
@@ -21,18 +21,21 @@ data["price_difference_user"] = data["visitor_hist_adr_usd"] - data["price_usd"]
 
 unique_prop = data.prop_id.unique()
 
-for prop in tqdm(unique_prop):
-    appearences = len(data[data.prop_id == prop]) #count appearences of a prop id
-    booked = len(data[(data.prop_id==prop)&(data.booking_bool==int(1))]) #count how many times a prop_id was booked
-    clicked = len(data[(data.prop_id==prop)&(data.click_bool==int(1))]) #count how many time a prop_id was clicked
-    data.loc[data.prop_id==prop,"prob_booked"] = booked/appearences #probability of being booked
-    data.loc[data.prop_id == prop, "prob_clicked"] = clicked/appearences #probability of being clicked
+#Run this part just if you need to change the code, since it takes too long to finish the loop
+# for prop in tqdm(unique_prop):
+#     appearences = len(data[data.prop_id == prop]) #count appearences of a prop id
+#     booked = len(data[(data.prop_id==prop)&(data.booking_bool==int(1))]) #count how many times a prop_id was booked
+#     clicked = len(data[(data.prop_id==prop)&(data.click_bool==int(1))]) #count how many time a prop_id was clicked
+#     data.loc[data.prop_id==prop,"prob_booked"] = booked/appearences #probability of being booked
+#     data.loc[data.prop_id == prop, "prob_clicked"] = clicked/appearences #probability of being clicked
+#
+# print(data)
+#
+# #Saving new data frame
+# print("Saving file to disk...")
+# data.to_pickle("feature_engineering.pkl") #save dataframe with new features
+#print("File saved!")
 
-print(data)
-
-#Saving new data frame
-print("Saving file to disk...")
-data.to_pickle("feature_engineering.pkl") #save dataframe with new features
-print("File saved!")
+#Load saved dataframe
 data = pd.read_pickle("feature_engineering.pkl")
 print(data)
