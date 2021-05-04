8e7891299bd2a6c3a6b8ecc35e4d03737d9ecdf2
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-03 22:58:18 -0300

    file saved

--- cleaning_data.py
+++ cleaning_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import pandas as ps
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pickle
 
 #load data
 data = pd.read_csv("Data/training_set_VU_DM.csv")
@@ -15,10 +17,22 @@ for column in data.columns:
 
 #Creating new features
 data["price_difference_history"] = data["prop_log_historical_price"] - np.log(data["price_usd"])
-data["price_difference_user"] = data["visitor_his_adr_usd"] - data["price_usd"]
+data["price_difference_user"] = data["visitor_hist_adr_usd"] - data["price_usd"]
 
-for prop in data["prop_id"].unique():
-    appearences = len(data[data.prop_id == prop])
-    booked = data[(data.prop_id==prop)&(data.booking_bool==int(1))]
-    data[data.prop_id==prop]["prob_booked"] = booked/appearences
+unique_prop = data.prop_id.unique()
 
+for prop in tqdm(unique_prop):
+    appearences = len(data[data.prop_id == prop]) #count appearences of a prop id
+    booked = len(data[(data.prop_id==prop)&(data.booking_bool==int(1))]) #count how many times a prop_id was booked
+    clicked = len(data[(data.prop_id==prop)&(data.click_bool==int(1))]) #count how many time a prop_id was clicked
+    data.loc[data.prop_id==prop,"prob_booked"] = booked/appearences #probability of being booked
+    data.loc[data.prop_id == prop, "prob_clicked"] = clicked/appearences #probability of being clicked
+
+print(data)
+
+#Saving new data frame
+print("Saving file to disk...")
+data.to_pickle("feature_engineering.pkl") #save dataframe with new features
+print("File saved!")
+data = pd.read_pickle("feature_engineering.pkl")
+print(data)
