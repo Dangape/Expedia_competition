3972cab08fb7bc3ed1da2f5d220b289e8a313554
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-05 13:19:44 -0300

    reviewed

--- cleaning_data.py
+++ cleaning_data.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pickle
 
-#load data
+# #load data
 data = pd.read_csv("Data/training_set_VU_DM.csv") #training data
 print(data.head())
 print(data.columns)
@@ -25,8 +25,10 @@ for prop in tqdm(unique_prop):
     data.loc[data.prop_id==prop,"prob_booked"] = booked/appearences #probability of being booked
     data.loc[data.prop_id == prop, "prob_clicked"] = clicked/appearences #probability of being clicked
 
-#Creating new features
-data = data.drop(columns=["gross_booking_usd"]) #not an interesting feature
+unique_prop = data.prop_id.unique() #unique property IDs
+
+# #Creating new features
+data = data.drop(columns=["gross_bookings_usd"]) #not an interesting feature
 data["price_difference_history"] = data["prop_log_historical_price"] - np.log(data["price_usd"])
 data["price_difference_user"] = data["visitor_hist_adr_usd"] - data["price_usd"]
 data["starrating_diff"] = data["visitor_hist_starrating"] - data["prop_starrating"]
@@ -39,24 +41,27 @@ for prop in tqdm(unique_prop):
     data.loc[data.prop_id == prop,"prop_review_score"] = data.loc[data.prop_id == prop,"prop_review_score"].fillna(min(data.loc[data.prop_id==prop,"prop_review_score"]))
     data.loc[data.prop_id == prop, "prop_location_score2"] = data.loc[data.prop_id == prop, "prop_location_score2"].fillna(min(data.loc[data.prop_id == prop, "prop_location_score2"]))
 
-unique_srch_dest = data.srch_destination_id.unique()
-unique_visitor_loc = data.visitor_location_country_id.unique()
 
-for destination in tqdm(unique_srch_dest):
-    for visitor in unique_visitor_loc:
-        data.loc[(data.srch_destination_id==destination)&(data.visitor_location_country_id==visitor),"orig_destination_distance"] = \
-            data.loc[(data.srch_destination_id==destination)&(data.visitor_location_country_id==visitor),"orig_destination_distance"].\
-                fillna(data.loc[(data.srch_destination_id==destination)&(data.visitor_location_country_id==visitor),"orig_destination_distance"].mean())
+# def replace_NaN(x):
+#     rows_NaN = x[x["orig_destination_distance"].isnull()]
+#     absolute_mean = x.orig_destination_distance.mean()
+#     for ind in tqdm(range(0,len(rows_NaN))):
+#         destination = x.loc[ind,"srch_destination_id"]
+#         visitor = x.loc[ind,"visitor_location_country_id"]
+#         filter = x.query('srch_destination_id==@destination & visitor_location_country_id== @visitor')
+#         mean = x.query('srch_destination_id==@destination & visitor_location_country_id== @visitor').mean()
+#         if len(filter)>1:
+#             filter["orig_destination_distance"] = filter["orig_destination_distance"].fillna(mean)
+#         else:
+#             filter["orig_destination_distance"] = filter["orig_destination_distance"].fillna(absolute_mean)
+#     return x
+#
+# data = replace_NaN(data)
+# print(data)
 
-data["orig_destination_distance"] = data["orig_destination_distance"].fillna(data["orig_destination_distance"].mean()) #replace remaining NAs with mean
+data["orig_destination_distance"] = data["orig_destination_distance"].fillna(data["orig_destination_distance"].mean())
 
 #Saving new data frame
 print("Saving file to disk...")
 data.to_pickle("feature_engineering.pkl") #save dataframe with new features
 print("File saved!")
-
-#Load saved dataframe
-print("Loading data...")
-data = pd.read_pickle("feature_engineering.pkl")
-print("Loaded!")
-print(data)
