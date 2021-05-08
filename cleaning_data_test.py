07758a380342169faf205d6be59d013cb3457df4
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-07 21:43:50 -0300

    Update cleaning_data_test.py

--- cleaning_data_test.py
+++ cleaning_data_test.py
@@ -32,27 +32,27 @@ for prop in tqdm(unique_prop):
     data.loc[data.prop_id == prop,"prop_review_score"] = data.loc[data.prop_id == prop,"prop_review_score"].fillna(data.loc[data.prop_id==prop,"prop_review_score"].min())
     data.loc[data.prop_id == prop, "prop_location_score2"] = data.loc[data.prop_id == prop, "prop_location_score2"].fillna(data.loc[data.prop_id == prop, "prop_location_score2"].min())
 
-for i in list(data.columns)[26:50]:
+for i in list(data.columns)[25:50]:
     data[str(i)] = data[str(i)].fillna(0) #replaceing competitors variables
 
 
-# def replace_NaN(x):
-#     rows_NaN = x[x["orig_destination_distance"].isnull()]
-#     absolute_mean = x.orig_destination_distance.mean()
-#     for ind in tqdm(range(0,len(rows_NaN))):
-#         destination = x.loc[ind,"srch_destination_id"]
-#         visitor = x.loc[ind,"visitor_location_country_id"]
-#         filter = x.query('srch_destination_id==@destination & visitor_location_country_id== @visitor')
-#         mean = x.query('srch_destination_id==@destination & visitor_location_country_id== @visitor').mean()
-#         if len(filter)>1:
-#             filter["orig_destination_distance"] = filter["orig_destination_distance"].fillna(mean)
-#         else:
-#             filter["orig_destination_distance"] = filter["orig_destination_distance"].fillna(absolute_mean)
-#     return x
+# # def replace_NaN(x):
+# #     rows_NaN = x[x["orig_destination_distance"].isnull()]
+# #     absolute_mean = x.orig_destination_distance.mean()
+# #     for ind in tqdm(range(0,len(rows_NaN))):
+# #         destination = x.loc[ind,"srch_destination_id"]
+# #         visitor = x.loc[ind,"visitor_location_country_id"]
+# #         filter = x.query('srch_destination_id==@destination & visitor_location_country_id== @visitor')
+# #         mean = x.query('srch_destination_id==@destination & visitor_location_country_id== @visitor').mean()
+# #         if len(filter)>1:
+# #             filter["orig_destination_distance"] = filter["orig_destination_distance"].fillna(mean)
+# #         else:
+# #             filter["orig_destination_distance"] = filter["orig_destination_distance"].fillna(absolute_mean)
+# #     return x
+# #
+# # data = replace_NaN(data)
+# # print(data)
 #
-# data = replace_NaN(data)
-# print(data)
-
 data["orig_destination_distance"] = data["orig_destination_distance"].fillna(data["orig_destination_distance"].mean())
 data["prop_review_score"] = data["prop_review_score"].fillna(data["prop_review_score"].mean()) #Replace ramaining NAs
 data["prop_location_score2"] = data["prop_location_score2"].fillna(data["prop_location_score2"].mean()) #replacing remaining 3% NAs
@@ -60,7 +60,8 @@ data["prop_location_score2"] = data["prop_location_score2"].fillna(data["prop_lo
 for column in data.columns:
     percent = (data[str(column)].isna().sum())/(len(data[str(column)]))*100
     print(str(column),":",float(percent))
-#
+
+
 #Saving new data frame
 print("Saving file to disk...")
 data.to_csv("feature_engineering_test.csv",index=False) #save dataframe with new features
