49502cb13a42244b5515403f4de52c3d8f358492
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-05-15 15:26:29 -0300

    stacking

--- modeling.py
+++ modeling.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
+from sklearn.linear_model import Ridge, SGDRegressor
+from sklearn.svm import SVR
 from xgboost import XGBRegressor
 import pickle
 import time
@@ -35,25 +36,71 @@ print(len(X.columns))
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
 #X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
 
-print("Started running model...")
-# #Create Random forest model
-# print("Random Forest...")
-# forest = RandomForestClassifier(n_estimators=200, random_state=0)
-# classifier_rf = MultiOutputClassifier(forest)
-# classifier_rf.fit(X_train, Y_train)
-# score = classifier_rf.score(X_test,Y_test)
-# print(score)
-#
-# print("Saving model to disk...")
-# filename = 'finalized_model_rf.sav'
-# pickle.dump(classifier_rf, open(filename, 'wb'))
-# print("Model saved!")
+#Create Ridge Regressor model
+print("Ridge model...")
+regressor = Ridge(alpha=100)
+classifier_ridge = MultiOutputRegressor(regressor)
+classifier_ridge.fit(X_train, Y_train)
+
+y_train_pred1 = classifier_ridge.predict(X_train)
+y_pred1 = classifier_ridge.predict(X_test)
+train_mse1 = mean_squared_error(y_train_pred1, Y_train)
+test_mse1 = mean_squared_error(y_pred1, Y_test)
+train_rmse1 = np.sqrt(train_mse1)
+test_rmse1 = np.sqrt(test_mse1)
+print('Train RMSE: %.4f' % train_rmse1)
+print('Test RMSE: %.4f' % test_rmse1)
+
+print("Saving model to disk...")
+filename = 'finalized_model_ridge.sav'
+pickle.dump(classifier_ridge, open(filename, 'wb'))
+print("Model saved!")
+
+#Create SGD Regressor model
+print("SGD Regressor...")
+regressor = SGDRegressor(eta0=0.01,epsilon=0.0001,loss='squared_epsilon_insensitive',learning_rate='adaptative')
+classifier_SGD = MultiOutputRegressor(regressor)
+classifier_SGD.fit(X_train, Y_train)
+
+y_train_pred1 = classifier_SGD.predict(X_train)
+y_pred1 = classifier_SGD.predict(X_test)
+train_mse1 = mean_squared_error(y_train_pred1, Y_train)
+test_mse1 = mean_squared_error(y_pred1, Y_test)
+train_rmse1 = np.sqrt(train_mse1)
+test_rmse1 = np.sqrt(test_mse1)
+print('Train RMSE: %.4f' % train_rmse1)
+print('Test RMSE: %.4f' % test_rmse1)
+
+print("Saving model to disk...")
+filename = 'finalized_model_SGD.sav'
+pickle.dump(classifier_SGD, open(filename, 'wb'))
+print("Model saved!")
+
+#Create SVR model
+print("SVR...")
+regressor = SVR(epsilon=0.2)
+classifier_SVR = MultiOutputRegressor(regressor)
+classifier_SVR.fit(X_train, Y_train)
+
+y_train_pred1 = classifier_SVR.predict(X_train)
+y_pred1 = classifier_SVR.predict(X_test)
+train_mse1 = mean_squared_error(y_train_pred1, Y_train)
+test_mse1 = mean_squared_error(y_pred1, Y_test)
+train_rmse1 = np.sqrt(train_mse1)
+test_rmse1 = np.sqrt(test_mse1)
+print('Train RMSE: %.4f' % train_rmse1)
+print('Test RMSE: %.4f' % test_rmse1)
+
+print("Saving model to disk...")
+filename = 'finalized_model_SVR.sav'
+pickle.dump(classifier_SVR, open(filename, 'wb'))
+print("Model saved!")
 
 #create new a XBC model
 print("XBC...")
 eval_set = [(X_test,Y_test)]
 XBC = XGBRegressor(booster = 'gbtree',learning_rate =0.01,
- n_estimators=5000,max_depth=6,gamma=0.1,eval_metric="rmse",
+ n_estimators=5000,max_depth=4,gamma=0.2,eval_metric="rmse",
                    early_stopping_rounds = 50,eval_set=eval_set,verbose=True)
 classifier_XBC = MultiOutputRegressor(XBC)
 classifier_XBC.fit(X_train, Y_train)
@@ -63,15 +110,9 @@ filename = 'finalized_model_XBC.sav'
 pickle.dump(classifier_XBC, open(filename, 'wb'))
 print("Model saved!")
 
-# print("Loading model...")
-# filename = 'finalized_model_XBC.sav'
-# classifier_XBC = pickle.load(open(filename, 'rb'))
-
 # evaluate model
 y_train_pred1 = classifier_XBC.predict(X_train)
 y_pred1 = classifier_XBC.predict(X_test)
-
-
 train_mse1 = mean_squared_error(y_train_pred1, Y_train)
 test_mse1 = mean_squared_error(y_pred1, Y_test)
 train_rmse1 = np.sqrt(train_mse1)
@@ -79,16 +120,39 @@ test_rmse1 = np.sqrt(test_mse1)
 print('Train RMSE: %.4f' % train_rmse1)
 print('Test RMSE: %.4f' % test_rmse1)
 
-print("Importance")
+print("Importance XBC...")
 feature_importances = pd.DataFrame(classifier_XBC.estimators_[0].feature_importances_,index = X_train.columns,columns=['importance']).sort_values('importance')
 print(feature_importances)
 figure(num=None, figsize=(20,18), dpi=80, facecolor='w', edgecolor='r')
 sns.barplot(x= feature_importances.importance,y =feature_importances.index)
-plt.title("Feature importance")
-plt.savefig("importance.png")
+plt.title("Feature importance XGBoost",fontsize=50)
+plt.savefig("importance_XBC.png")
 plt.show()
 
-# imp = OrderedDict(sorted(classifier_XBC.get_booster().get_fscore().items(), key=lambda t: t[1], reverse=True))
-# print("Importance:",imp)
+print("Importance Ridge...")
+feature_importances = pd.DataFrame(classifier_ridge.estimators_[0].feature_importances_,index = X_train.columns,columns=['importance']).sort_values('importance')
+print(feature_importances)
+figure(num=None, figsize=(20,18), dpi=80, facecolor='w', edgecolor='r')
+sns.barplot(x= feature_importances.importance,y =feature_importances.index)
+plt.title("Feature importance Ridge",fontsize=50)
+plt.savefig("importance_ridge.png")
+plt.show()
 
+print("Importance SGD..")
+feature_importances = pd.DataFrame(classifier_SGD.estimators_[0].feature_importances_,index = X_train.columns,columns=['importance']).sort_values('importance')
+print(feature_importances)
+figure(num=None, figsize=(20,18), dpi=80, facecolor='w', edgecolor='r')
+sns.barplot(x= feature_importances.importance,y =feature_importances.index)
+plt.title("Feature importance SGD",fontsize=50)
+plt.savefig("importance_SGD.png")
+plt.show()
+
+print("Importance SVR...")
+feature_importances = pd.DataFrame(classifier_SVR.estimators_[0].feature_importances_,index = X_train.columns,columns=['importance']).sort_values('importance')
+print(feature_importances)
+figure(num=None, figsize=(20,18), dpi=80, facecolor='w', edgecolor='r')
+sns.barplot(x= feature_importances.importance,y =feature_importances.index)
+plt.title("Feature importance SVR",fontsize=50)
+plt.savefig("importance_SVR.png")
+plt.show()
 print("%s minutes" %((time.time() - start_time)/60))
